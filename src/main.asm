1935f7396bf8bec2ff774b5e4bb5450d43b57df8
rahul4732saini <rahul4732saini@gmail.com>
2025-06-22 15:19:36 +0530

    Define setup routine for game environment setup
    Defined the routine to setup the segment registers and switch to 320x200
    VGA mode to effectively draw the game components on the screen.

--- src/main.asm
+++ src/main.asm
@@ -1,5 +1,23 @@
 BITS    16
 ORG     0x7C00
 
+setup:
+    MOV     ax, 0
+    MOV     ds, ax
+
+    ; Sets up the Extra Segment to access the VGA
+    ; mapped memory region (0xA0000 - 0xAFFFF).
+    MOV     ax, 0xA000
+    MOV     es, ax
+
+    ; The memory region (0x7E00 - 0x17DFF) is used
+    ; for the Stack Segment.
+    MOV     ax, 0x7E0
+    MOV     ss, ax
+
+    MOV     ax, 0xFFFF
+    MOV     bp, ax
+    MOV     sp, ax
+
 DB  510 - ($ - $$)  dup(0)
 DW  0xAA55
